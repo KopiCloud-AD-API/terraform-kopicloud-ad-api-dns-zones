340c6c07bd823ac31bf289d19a7db03715e383a5
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-02-24 00:39:58 +0100

    Initial commit

--- dns-zones-lookup-create.tf
+++ dns-zones-lookup-create.tf
@@ -7,7 +7,7 @@ resource "kopicloud_dns_lookup_zone" "test_lookup" {
   zone_name = "kopicloud.org"
 }
 
-# Returns Create DNS Lookup Zone
+# Returns Created DNS Lookup Zone
 output "dns_lookup_zone" {
   description = "Created DNS Lookup Zone"
   value       = resource.kopicloud_dns_lookup_zone.test_lookup
