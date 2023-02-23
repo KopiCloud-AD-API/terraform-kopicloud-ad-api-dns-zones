d601ba872aa066613e67ee84ac4cfcc3cceeff3b
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-02-23 12:53:31 +0100

    Initial commit

---
+++ dns-zones-lookup-create.tf
@@ -0,0 +1,14 @@
+###############################################
+## KopiCloud AD API - Create DNS Lookup Zone ##
+###############################################
+
+# Create a DNS Lookup Zone
+resource "kopicloud_dns_lookup_zone" "test_lookup" {
+  zone_name = "kopicloud.org"
+}
+
+# Returns Create DNS Lookup Zone
+output "dns_lookup_zone" {
+  description = "Created DNS Lookup Zone"
+  value       = resource.kopicloud_dns_lookup_zone.test_lookup
+}
