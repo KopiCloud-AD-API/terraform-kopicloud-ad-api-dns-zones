d6c4c8f043836b062706b99eec1ec5892d67fc5c
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-04-02 18:46:38 +0200

    Initial commit

--- dns-zones-lookup-list.tf
+++ dns-zones-lookup-list.tf
@@ -7,7 +7,7 @@ data "kopicloud_dns_lookup_zone_list" "test_lookup_all" {
 }
 
 # Returns List of DNS Lookup Zones
-output "dns_lookup_zone_list" {
+output "OUTPUT_dns_lookup_zone_list" {
   description = "List of DNS Lookup Zones"
   value       = data.kopicloud_dns_lookup_zone_list.test_lookup_all
 }
