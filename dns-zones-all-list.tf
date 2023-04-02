9ddfb8d6f5c618a944fd13ba3c68fc5a1a316e95
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-04-02 19:12:20 +0200

    Initial commit

--- dns-zones-all-list.tf
+++ dns-zones-all-list.tf
@@ -6,7 +6,7 @@
 data "kopicloud_dns_zone_list" "test_all" { }
 
 # Returns List of All DNS Zones
-output "dns_all_zone_list" {
+output "OUTPUT_dns_all_zone_list" {
   description = "List of All DNS Zones"
   value       = data.kopicloud_dns_zone_list.test_all
 }
